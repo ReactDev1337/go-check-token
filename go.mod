8fd7eb000d3c4af2750a0103da1e201205c97c26
ReactDev1337 <82044444+ReactDev1337@users.noreply.github.com>
2021-04-07 01:04:37 +0200

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-check-token
+
+go 1.16
+
+require github.com/valyala/fasthttp v1.23.0 // indirect
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+
+	"github.com/valyala/fasthttp"
+)
+
+func checkToken(token string) {
+	tokenToCheck := token
+
+	req := fasthttp.AcquireRequest()
+	req.Header.Set("Authorization", tokenToCheck)
+	req.Header.SetMethod("GET")
+	req.SetRequestURI("https://canary.discordapp.com/api/v8/users/@me")
+	res := fasthttp.AcquireResponse()
+
+	fasthttp.Do(req, res)
+	fasthttp.ReleaseRequest(req)
+	statusCode := int(res.StatusCode())
+	fasthttp.ReleaseResponse(res)
+
+	if statusCode == 200 {
+		file, err := os.OpenFile("./output.txt", os.O_RDWR|os.O_APPEND, 0660)
+		if err != nil {
+			panic(err)
+		}
+
+		defer file.Close()
+		_, err = file.WriteString(tokenToCheck + "\n")
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func main() {
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		checkToken(scanner.Text())
+	}
+}
